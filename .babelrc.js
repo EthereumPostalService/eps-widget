e94826974880a706ef629ee00d80ece4d57e7980
sam <sammage@gmail.com>
2023-03-07 09:30:00 -0800

    format babel

--- .babelrc.js
+++ .babelrc.js
@@ -1,45 +1,45 @@
-const isDevelopment = process.env.NODE_ENV !== 'production';
+const isDevelopment = process.env.NODE_ENV !== "production";
 const envPreset = isDevelopment
-  ? ['@babel/preset-env', { loose: true }]
+  ? ["@babel/preset-env", { loose: true }]
   : [
-      '@babel/preset-env',
+      "@babel/preset-env",
       {
         modules: false,
         // Allow importing core-js in entrypoint and use browserlist to select polyfills
-        useBuiltIns: 'entry',
+        useBuiltIns: "entry",
         // Set the corejs version we are using to avoid warnings in console
         corejs: 3,
         // Exclude transforms that make all code slower
-        exclude: ['transform-typeof-symbol'],
+        exclude: ["transform-typeof-symbol"],
         loose: true,
-        "targets": {
-          "node": "current"
-        }
+        targets: {
+          node: "current",
+        },
       },
     ];
 
 module.exports = {
-  'presets': [
+  presets: [
     [
-      '@babel/preset-react',
+      "@babel/preset-react",
       {
         development: isDevelopment,
       },
     ],
     [
-      '@babel/preset-typescript',
+      "@babel/preset-typescript",
       {
-        'isTSX': true,
-        'allExtensions': true,
+        isTSX: true,
+        allExtensions: true,
       },
     ],
     envPreset,
   ],
-  'plugins': [
-    ['@babel/plugin-proposal-class-properties', { 'loose': true }],
-    '@babel/plugin-syntax-dynamic-import',
+  plugins: [
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    "@babel/plugin-syntax-dynamic-import",
     [
-      '@babel/plugin-transform-runtime',
+      "@babel/plugin-transform-runtime",
       {
         corejs: false,
         helpers: !isDevelopment,
@@ -48,11 +48,11 @@ module.exports = {
       },
     ],
   ],
-  'env': {
+  env: {
     production: {
-      'plugins': [
+      plugins: [
         [
-          'babel-plugin-transform-react-remove-prop-types',
+          "babel-plugin-transform-react-remove-prop-types",
           {
             removeImport: true,
           },
